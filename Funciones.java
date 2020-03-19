c40bd4372235fe7d25d4484b2dc4e7394766f046
joserivera93 <josemanuelrr93@gmail.com>
2020-03-19 15:13:21 +0100

    #15 Se ha añadido la Funcion_6 en este archivo

--- Funciones.java
+++ Funciones.java
@@ -1,12 +1,11 @@
 public class Funciones {
 
-	public static void main(String[] args) {
+	    public static void main(String[] args) {
+
+                double [] num1 = {2,8,16,48};
 
 
-                int [] numero = {2,8,16,48};
-
-
-		System.out.println(Funcion5(numero));
+		System.out.println(Funcion6(num1));
 	}
 
 	public static Boolean Funcion1(int num) {
@@ -78,7 +77,7 @@ public class Funciones {
 		return CadenaConcatenada;
 
 	}
-	public int Funcion5(int [] num) {
+	public static int Funcion5(int [] num) {
 
 		int tam = num.length;
 		int sum = 0; 
@@ -92,4 +91,27 @@ public class Funciones {
 		return sum;
 
 	}
+
+	public static double Funcion6(double [] num1) {
+
+        	double [] num2 = new double [num1.length];
+        	double z = 4;
+        	double w = 4;
+
+
+	        for (int cont = 0; cont < num1.length; cont++) {
+        	    if (num1[cont]%2==0) {
+                	num2[cont] = num1[cont]*z; 
+                	System.out.print("  Numero Par -->  " + num2[cont]);
+            	}else {
+               		num2[cont] = num1[cont]*w; 
+                	System.out.print("  Numero Impar -->  " +num2[cont]);
+            }
+        }
+
+
+		return z;
+
+
+        }
 }
