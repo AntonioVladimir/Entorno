aa8f9e2e87af6bcf89922c773ae6a17cc0b90ab3
Berny13 <bernyjimenezsanchez@gmail.com>
2020-03-20 12:22:35 +0100

    #19 Archivo Funciones

--- Funciones.java
+++ Funciones.java
@@ -1,8 +1,8 @@
 public class Funciones {
-	
-	public static void main(String[] args) {
 
-		System.out.println(Funcion2(258));
+public static void main(String[] args) {
+
+		System.out.println(Funcion1(98));
 	}
 	
 	public static Boolean Funcion1(int num) {
@@ -20,26 +20,4 @@ public class Funciones {
 		}
 
 		return resp;
-
-	}
-
-
-	public static Boolean Funcion2(int num) {
-
-		Boolean resp = null;
-
-		int y = 250 - 50;
-		int x = 250 + 50;
-
-		if (num <= x || num >= y) {
-
-			resp = true;
-
-		} else {
-			resp = false;
-		}
-
-		return resp;
-
-	}
 }
