e8e55117faef391ec73df9dd48d965934d7be468
AntonioVladimir <56442603+AntonioVladimir@users.noreply.github.com>
2020-03-22 03:55:43 +0100

    #39 Prueba de alumnos nulos y tiempos vacíos.

--- TestFuncion8.java
+++ TestFuncion8.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -8,69 +9,71 @@ import org.junit.jupiter.api.Test;
 
 public class TestFuncion8 {
 
+	static int contpruebas = 0;
+	static Funciones func = null;
 
-			static int contpruebas = 0;
-			static Funciones func = null;
+	@BeforeAll
+	static void prepararPruebas() {
+		System.out.println("Empezamos las Pruebas.");
+		func = new Funciones();
+	}
 
-			@BeforeAll
-			static void prepararPruebas() {
-				System.out.println("Empezamos las Pruebas.");
-				func = new Funciones();
-			}
+	@BeforeEach
+	private void contador() {
+		contpruebas++;
+		System.out.println("Vamos a realizar la prueba " + contpruebas);
+	}
 
-			@BeforeEach
-			private void contador() {
-				contpruebas++;
-				System.out.println("Vamos a realizar la prueba "+contpruebas);
-			}
+	@AfterEach
+	private void vecespruebas() {
+		System.out.println("Se a realizado " + contpruebas + " Pruebas.");
+	}
 
-			@AfterEach
-			private void vecespruebas() {
-				System.out.println("Se a realizado "+ contpruebas +" Pruebas.");
-			}
+	@AfterAll
+	static void terminarPruebas() {
+		contpruebas = 0;
+		func = null;
+		System.out.println("Se han terminado de ejecutar todas las pruebas");
+	}
 
-			@AfterAll
-			static void terminarPruebas() {
-				contpruebas = 0;
-				func = null;
-				System.out.println("Se han terminado de ejecutar todas las pruebas");
-			}
+	@Test // Pruebas realizadas por Jose Manuel
+	@DisplayName("Test de Caja Negra Comprobación de Rango.")
+	void testFalse() {// suma los tiempos
 
-			@Test//Pruebas realizadas por Jose Manuel
-			@DisplayName("Test de Caja Negra Comprobación de Rango.")
-			void testFalse() {//suma los tiempos
+		//
+		String[] alumnos = { "Antonio", "Gabri", "Jorge", "Azahara" };
+		int[][] tiemposDeTrabajo = { { 5, 6, 7, 8 }, { 1, 2, 3, 4 } };
 
-				//
-				String[] alumnos = { "Antonio", "Gabri", "Jorge", "Azahara" };
-				int[][] tiemposDeTrabajo = {{5,6,7,8},{1,2,3,4}} ;
+		// Nos saldra False ya que la suma es mayor a 500
+		assertFalse(func.Funcion8(alumnos, tiemposDeTrabajo));
+	}
 
+	@Test
+	@DisplayName("Test de Caja Negra Comprobación suma tiempo")
+	void testTrue() {// suma los tiempo
 
-				//Nos saldra False ya que la suma es mayor a 500
-				assertFalse(func.Funcion8(alumnos, tiemposDeTrabajo));
-			}
+		// Nos saldra true ya que la suma es mayor a 500
+		String[] alumnos = { "Antonio", "Gabri", "Jorge", "Azahara" };
+		int[][] tiemposDeTrabajo = { { 500, 650, 720, 800 }, { 100, 200, 350, 490 } };
 
-			@Test
-			@DisplayName("Test de Caja Negra Comprobación suma tiempo")
-			void testTrue() {//suma los tiempo 
+		assertTrue(func.Funcion8(alumnos, tiemposDeTrabajo));
+	}
 
-				//Nos saldra true ya que la suma es mayor a 500
-				String[] alumnos = { "Antonio", "Gabri", "Jorge", "Azahara" };
-				int[][] tiemposDeTrabajo = {{500,650,720,800},{100,200,350,490}};
+	// Pruebas realizadas por Rafael Oliva Ramirez
+	@Test
+	@DisplayName("Test de Caja Negra Comprobación salida no nula")
+	void testNotNull() {
 
-				assertTrue(func.Funcion8(alumnos, tiemposDeTrabajo));	
-		}
+		String[] alumnos = { "Rafael", "Jose", "Vladi", "Berny" };
+		int[][] tiemposDeTrabajos = { { 1, 2, 3, 4 }, { 54, 65, 89, 78 } };
 
-	//Pruebas realizadas por Rafael Oliva Ramirez
-		@Test
-		@DisplayName("Test de Caja Negra Comprobación salida no nula")
-		void testNotNull() {
+		assertNotNull(func.Funcion8(alumnos, tiemposDeTrabajos));
+	}
 
-			String [] alumnos = {"Rafael","Jose","Vladi","Berny"};
-	        int[][] tiemposDeTrabajos = {{1,2,3,4} , {54,65,89,78}};
-
-			assertNotNull(func.Funcion8(alumnos, tiemposDeTrabajos));	
-		}
-
+	// Pruebas realizadas por Antonio Vladimir Ortega Chinchilla
+	@DisplayName("Test de Caja Negra alumnos y tiempos de trabajo vacios")
+	@Test
+	void testListaNula() {
+		assertFalse(null, false);
+	}
 }
-
-
